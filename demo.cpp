6eb74f6e28f5c393598b85152836bbbc75e99085
Jack Chen <redchenjs@live.com>
2023-10-10 21:27:35 +0800

    demo: init commit

---
+++ demo.cpp
@@ -0,0 +1,200 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <thread>
+#include <time.h>
+#include <unistd.h>
+#include <csignal>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+using namespace std;
+
+#define FRAME_CNT    (60)
+#define FRAME_FPS    (20)
+#define FRAME_PKT    (1442)
+#define FRAME_SYN    (2879)
+
+#define FRAME_WIDTH  (1920)
+#define FRAME_HEIGHT (1080)
+
+bool running = false;
+
+mutex           frame_mutex;
+queue<cv::Mat>  frame_queue;
+queue<uint16_t> count_queue;
+
+void signal_handle(int signum)
+{
+    switch (signum) {
+    case SIGINT:
+    case SIGTERM:
+        running = false;
+        break;
+    default:
+        break;
+    }
+}
+
+void rgb565_bgr888(uint8_t *dest, uint16_t *src, int size)
+{
+    for (int i = 0; i < size; i++) {
+        uint16_t rgb16 = *src++;
+
+        dest[i * 3 + 2] = (rgb16 & 0xf800) >> 8;
+        dest[i * 3 + 1] = (rgb16 & 0x07e0) >> 3;
+        dest[i * 3 + 0] = (rgb16 & 0x001f) << 3;
+    }
+}
+
+void t1_getframe(void)
+{
+    int ret = 0;
+    int sock_fd = 0;
+    socklen_t src_len = 0;
+    struct sockaddr_in src_addr = { 0 };
+
+    uint16_t pkt_idx = 0;
+    uint8_t  pkt_buff[FRAME_PKT] = {0};
+
+    uint16_t count_curr = 0;
+
+    uint8_t  frame_curr = 0;
+    bool     frame_sync = false;
+    cv::Mat  frame_buff[FRAME_CNT];
+
+    cout << "T1: 视频接收线程...启动" << endl;
+
+    if ((sock_fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        printf("T1: 故障：无法创建套接字\n");
+        return;
+    }
+
+    src_addr.sin_family = AF_INET;
+    src_addr.sin_addr.s_addr = inet_addr("192.168.1.102");
+    src_addr.sin_port = htons(8001);
+
+    if ((ret = bind(sock_fd, (sockaddr *)&src_addr, sizeof(src_addr))) < 0) {
+        printf("T1: 故障：无法绑定端口\n");
+        return;
+    };
+
+    for (int i = 0; i < FRAME_CNT; i++) {
+        frame_buff[i] = cv::Mat(cv::Size(FRAME_WIDTH, FRAME_HEIGHT), CV_8UC3, cv::Scalar(0, 0, 0));
+    }
+
+    while (running) {
+        while (true) {
+            if ((ret = recvfrom(sock_fd, pkt_buff, FRAME_PKT, 0, (sockaddr *)&src_addr, &src_len)) < 0) {
+                printf("T1: 故障: %s\n", strerror(errno));
+            }
+
+            pkt_idx = pkt_buff[0] << 8 | pkt_buff[1];
+
+            if (pkt_idx == 0) {
+                if (!frame_sync) {
+                    count_curr = 0;
+                    frame_sync = true;
+                }
+            } else {
+                if (!frame_sync) {
+                    continue;
+                }
+            }
+
+            rgb565_bgr888(frame_buff[frame_curr].data + pkt_idx * (FRAME_PKT - 2) / 2 * 3, (uint16_t *)&pkt_buff[2], (FRAME_PKT - 2) / 2);
+
+            count_curr++;
+
+            if (frame_sync && (pkt_idx == FRAME_SYN)) {
+                break;
+            }
+        }
+
+        frame_mutex.lock();
+        count_queue.push(count_curr);
+        frame_queue.push(frame_buff[frame_curr]);
+        frame_mutex.unlock();
+
+        count_curr = 0;
+
+        frame_curr = (++frame_curr) % FRAME_CNT;
+        frame_buff[frame_curr].setTo(cv::Scalar(0, 0, 0));
+
+        // usleep(1000 * 1000 / FRAME_FPS);
+    }
+
+    cout << "T1: 视频接收线程...关闭" << endl;
+}
+
+void t2_showframe(void)
+{
+    int fps = 0;
+    uint16_t count_curr;
+    cv::Mat frame_buff(cv::Size(FRAME_WIDTH, FRAME_HEIGHT), CV_8UC3, cv::Scalar(0, 0, 0));
+    struct timespec start  = {0, 0};
+    struct timespec finish = {0, 0};
+
+    cout << "T2: 视频显示线程...启动" << endl;
+
+	cv::namedWindow("frame");
+
+	while (running) {
+		if (!frame_queue.empty()) {
+            clock_gettime(CLOCK_REALTIME, &start);
+
+			frame_mutex.lock();
+			frame_buff = frame_queue.front();
+			frame_queue.pop();
+            count_curr = count_queue.front();
+            count_queue.pop();
+			frame_mutex.unlock();
+
+            if (!frame_buff.empty()) {
+                int err = (FRAME_SYN - count_curr) * 10000 / FRAME_SYN;
+
+                string s = "FPS:" + to_string(fps / 10) + "." + to_string(fps % 10);
+                cv::putText(frame_buff, s.c_str(), cv::Point(10, 30), cv::FONT_HERSHEY_COMPLEX, 1.0, cv::Scalar(0, 255, 255), 2, 8, 0);
+                string e = "ERR:" + to_string(err / 100) + "." + to_string(err % 100) + "%";
+                cv::putText(frame_buff, e.c_str(), cv::Point(10, 60), cv::FONT_HERSHEY_COMPLEX, 1.0, cv::Scalar(0, 0, 255), 2, 8, 0);
+
+                cv::imshow("frame", frame_buff);
+            } else {
+                usleep(1000);
+                continue;
+            }
+
+            cv::waitKey(1000 / FRAME_FPS);
+
+            clock_gettime(CLOCK_REALTIME, &finish);
+
+            fps = (double)10000 / ((finish.tv_sec - start.tv_sec) * 1000 + (finish.tv_nsec - start.tv_nsec) / 1000000);
+		}
+	}
+
+	cv::destroyWindow("frame");
+
+    cout << "T2: 视频显示线程...关闭" << endl;
+}
+
+int main()
+{
+    signal(SIGINT, signal_handle);
+    signal(SIGTERM, signal_handle);
+
+    cout << "M : 视频处理进程...启动" << endl;
+
+    running = true;
+
+	thread t1(t1_getframe);
+	t1.detach();
+	thread t2(t2_showframe);
+	t2.join();
+    t1.join();
+
+    cout << "M : 视频处理进程...关闭" << endl;
+
+	return 0;
+}
